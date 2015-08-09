c00e84edd36c461ae6b54812769779f0bdb4c36c
mswift42 <martin.haesler@gmail.com>
2015-08-09 14:13:39 +0100

    Add getters and setters.

---
+++ mooctype/cookbook/CookBook/scripts/baseRecipeCategory.ts
@@ -0,0 +1,23 @@
+﻿class BaseRecipeCategory {
+    private _name: string;
+    private _foodGroups: FoodGroup[] = [];
+
+    //TODO: Create get and set blocks for each of the variables in the declaration above.
+
+
+    get foodGroups():FoodGroup[] {
+        return this._foodGroups;
+    }
+
+    set foodGroups(value:Array) {
+        this._foodGroups = value;
+    }
+
+    get name():string {
+        return this._name;
+    }
+
+    set name(value:string) {
+        this._name = value;
+    }
+}
