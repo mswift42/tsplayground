a953bafee8cc9b3cc3135f2be6759943b95805f9
mswift42 <martin.haesler@gmail.com>
2015-08-16 13:24:57 +0100

    Add constructor for recipiCategory.

--- mooctype/mod_6/CookBook/scripts/baseRecipeCategory.ts
+++ mooctype/mod_6/CookBook/scripts/baseRecipeCategory.ts
@@ -3,8 +3,9 @@
     foodGroups: FoodGroup[] = [];
 
     //TODO:
-    constructor(name:string) {
+    constructor(name:string, foodGroups: FoodGroup[]) {
         this.name = name;
+        this.foodGroups = foodGroups;
     }
     //Add constructor using the variable names above
     //as the parameters (so you'll have two parameters). 
