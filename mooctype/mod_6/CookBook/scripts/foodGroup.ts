0cbb4ecb9de1459dac8dd0cb47675bb13c9edb2a
mswift42 <martin.haesler@gmail.com>
2015-08-16 13:26:05 +0100

    initialize name parameter.

---
+++ mooctype/mod_6/CookBook/scripts/foodGroup.ts
@@ -0,0 +1,12 @@
+﻿class FoodGroup {
+    name: string;
+
+    constructor(name: string) {
+        //TODO
+        this.name = name;
+        //Assign the "name" parameter to the 
+        //"name" member variable 
+
+
+    }  
+} 
