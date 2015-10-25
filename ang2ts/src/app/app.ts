3426a483077457bddb1253472005c01807ea0288
mswift42 <martin.haesler@gmail.com>
2015-10-25 08:25:20 +0000

    upgrade angular2 0.0 44

---
+++ ang2ts/src/app/app.ts
@@ -0,0 +1,10 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My Second Angular2 App!'
+})
+
+class AppComponent {}
+
+bootstrap(AppComponent)
