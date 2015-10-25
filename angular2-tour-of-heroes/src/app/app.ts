c32d042ba2254443af7d6f11a9f0f47cd5ee2186
mswift42 <martin.haesler@gmail.com>
2015-10-25 08:46:44 +0000

    Enable twoway-binding for hero.name

--- angular2-tour-of-heroes/src/app/app.ts
+++ angular2-tour-of-heroes/src/app/app.ts
@@ -1,13 +1,29 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {Component, bootstrap, FORM_DIRECTIVES} from 'angular2/angular2';
+
+class Hero {
+    id: number;
+    name: string;
+}
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+    template:`
+<h1>{{title}}</h1>
+<h2>{{hero.name}} details!</h2>
+<div><label>id: </label>{{hero.id}}</div>
+<div>
+<label>name: </label>
+<div><input [(ng-model)]="hero.name" placeholder="name"></div>
+</div>` ,
+    directives: [FORM_DIRECTIVES]
 })
 
 class AppComponent {
     public title = 'Tour of Heroes';
-    public hero = 'Windstorm';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
 }
 
 bootstrap(AppComponent)
