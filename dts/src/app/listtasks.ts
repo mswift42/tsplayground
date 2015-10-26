d5c8781f5f7ed2d8fb04af48c608ee030ecc1df6
mswift42 <martin.haesler@gmail.com>
2015-10-26 18:02:51 +0000

    Use TaskService to display array of single tasks.

--- dts/src/app/listtasks.ts
+++ dts/src/app/listtasks.ts
@@ -1,3 +1,18 @@
-import {Component} from 'angular2/angular2';
+import {Component, NgFor} from 'angular2/angular2';
 import {TaskService} from './Taskservice';
 import {Task} from './Task';
+
+@Component({
+  selector: 'list-tasks',
+  templateUrl: 'app/listtasks.html',
+  directives: [NgFor]
+})
+
+export class ListTasks {
+  ts: TaskService;
+  tasklist: Array<Task>;
+  constructor(TaskService: TaskService) {
+    this.ts = TaskService;
+    this.tasklist = this.ts.getTasks();
+  }
+}
