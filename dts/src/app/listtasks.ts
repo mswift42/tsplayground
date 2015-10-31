999ecbe6823b469516cc66ad9527657eefeb6929
mswift42 <martin.haesler@gmail.com>
2015-10-31 12:29:04 +0000

    Import listtask element

--- dts/src/app/listtasks.ts
+++ dts/src/app/listtasks.ts
@@ -1,16 +1,22 @@
 import {Component, NgFor} from 'angular2/angular2';
 import {TaskService} from './Taskservice';
 import {Task} from './Task';
+import {ListTask} from './listtask';
 
 @Component({
-  selector: 'list-tasks',
-  templateUrl: 'app/listtasks.html',
-  directives: [NgFor]
+    selector: 'list-tasks',
+    templateUrl: 'app/listtasks.html',
+    directives: [NgFor, ListTask]
 })
 
 export class ListTasks {
-  tasklist: Array<Task>;
-  constructor(TaskService: TaskService) {
-    this.tasklist = TaskService.getTasks();
-  }
+    tasklist: Array<Task>;
+    editing: boolean;
+    constructor(TaskService: TaskService) {
+        this.tasklist = TaskService.getTasks();
+        this.editing = true;
+    }
+    toggleEdit() {
+        this.editing = !this.editing;
+    }
 }
