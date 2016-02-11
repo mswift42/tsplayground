883ee02329dfa29c45a20b6a9d2955cd7987593f
mswift42 <martin.haesler@gmail.com>
2016-02-11 15:46:11 +0000

    add mock heroes.

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,13 +1,31 @@
 import {Component} from 'angular2/core';
+import {NgModel} from 'angular2/direc'
 
 interface Hero {
   id: number;
   name: string;
 }
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
 
 @Component({
     selector: 'my-app',
-template: '<h1>{{title}}</h1><h2>{{hero.name}} detail</h2>'
+template: `
+<h1>{{title}}</h1>
+ <h2>{{hero.name}} details!</h2>
+ <div><label>id: </label>{{hero.id}}</div>
+ <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+`
 })
 
 export class AppComponent {
