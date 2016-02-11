3a2020fe63ca9862395e8e06f451c03b2ff2cb27
mswift42 <martin.haesler@gmail.com>
2016-02-11 18:23:41 +0000

    add styles.

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -24,12 +24,59 @@ template: `
 <h2>My Heroes</h2>
 <ul class="heroes">
 <li *ngFor="#hero of heroes">
- <h2>{{hero.name}} details!</h2>
- <div><label>id: </label>{{hero.id}}</div>
- <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+ <span class="badge">{{hero.id}}</span> {{hero.name}}
  </li>
     </ul>
-`
+`,
+styles:[`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 10em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0em;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #EEE;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0em 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0px 0px 4px;
+  }
+`]
+
 })
 
 export class AppComponent {
