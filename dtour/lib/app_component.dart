46fa598ee1a0f1a5639bee14771190f3de5c3d2e
mswift42 <martin.haesler@gmail.com>
2016-02-13 16:31:20 +0000

    don't initialize selected Hero.

--- dtour/lib/app_component.dart
+++ dtour/lib/app_component.dart
@@ -2,27 +2,26 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/angular2.dart';
+import 'package:dtour/hero-detail_component.dart' show HeroDetailComponent;
+import 'package:dtour/Hero.dart' show Hero;
 
 @Component(
     selector: 'my-app',
     templateUrl: 'app_component.html',
-    styleUrls: const ['app_component.css'])
+    styleUrls: const ['app_component.css'],
+    directives: const [HeroDetailComponent]
+)
+
 class AppComponent {
   var title = 'Tour of Heroes';
   var heroes = HEROES;
   Hero selectedHero;
 
   void onSelect(Hero hero) {
-    print(hero.name);
     selectedHero = hero;
   }
 }
 
-class Hero {
-  num id;
-  String name;
-  Hero(this.id, this.name);
-}
 
 List<Hero> HEROES = [
   new Hero(11, "Mr. Nice"),
