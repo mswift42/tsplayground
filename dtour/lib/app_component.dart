b12acb138a4cd9ccd5f9a3f64a1d908068213d52
mswift42 <martin.haesler@gmail.com>
2016-02-13 11:40:36 +0000

    add css file

--- dtour/lib/app_component.dart
+++ dtour/lib/app_component.dart
@@ -3,10 +3,14 @@
 
 import 'package:angular2/angular2.dart';
 
-@Component(selector: 'my-app', templateUrl: 'app_component.html')
+@Component(
+    selector: 'my-app',
+    templateUrl: 'app_component.html',
+    styleUrls: const ['app_component.css'])
 class AppComponent {
   var title = 'Tour of Heroes';
   var hero = new Hero(1, 'Windstorm');
+  var heroes = HEROES;
 }
 
 class Hero {
@@ -14,15 +18,16 @@ class Hero {
   String name;
   Hero(this.id, this.name);
 }
+
 List<Hero> HEROES = [
-  new Hero(  11,  "Mr. Nice" ),
-  new Hero(  12,  "Narco" ),
-  new Hero(  13,  "Bombasto" ),
-  new Hero(  14,  "Celeritas" ),
-  new Hero(  15,  "Magneta" ),
-  new Hero(  16,  "RubberMan" ),
-  new Hero(  17,  "Dynama" ),
-  new Hero(  18,  "Dr IQ" ),
-  new Hero(  19,  "Magma" ),
-  new Hero(  20,  "Tornado" )
+  new Hero(11, "Mr. Nice"),
+  new Hero(12, "Narco"),
+  new Hero(13, "Bombasto"),
+  new Hero(14, "Celeritas"),
+  new Hero(15, "Magneta"),
+  new Hero(16, "RubberMan"),
+  new Hero(17, "Dynama"),
+  new Hero(18, "Dr IQ"),
+  new Hero(19, "Magma"),
+  new Hero(20, "Tornado")
 ];
