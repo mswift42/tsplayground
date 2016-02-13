d96bf6d597188cda4cb97b47cdf63b03bcd8383d
mswift42 <martin.haesler@gmail.com>
2016-02-13 08:25:21 +0000

    add ngOnInit method

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -2,12 +2,16 @@ import {Component} from 'angular2/core';
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import {DashboardComponent} from './dashboard.component';
 
 @Component({
   selector:'my-app',
     template: `
     <h1>{{title}}</h1>
+    <nav>
     <a [routerLink]="['Heroes']">Heroes</a>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    </nav>
     <router-outlet></router-outlet>`,
     directives: [ROUTER_DIRECTIVES],
     providers:[ROUTER_PROVIDERS,HeroService]
@@ -16,7 +20,13 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/route
   path: '/heroes',
   name: 'Heroes',
   component: HeroesComponent
-}])
+},
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  }])
 export class AppComponent {
   title = 'Tour of Heroes';
 }
--- angular2-tour-of-heroes/app/dashboard.component.ts
+++ angular2-tour-of-heroes/app/dashboard.component.ts
@@ -1,8 +1,19 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  templateUrl: 'app/dashboard.component.html'
 })
 
-export class DashboardComponent {}
+export class DashboardComponent {
+  heroes: Hero[] = [];
+  constructor(private _heroService: HeroService) {}
+
+  ngOnInit() {
+    this._heroService.getHeroes()
+    .then(heroes => this.heroes = heroes.slice(1,5));
+  }
+  gotoDetail() {}
+}
