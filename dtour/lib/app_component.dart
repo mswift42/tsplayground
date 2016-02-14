f912e6207e4459c0d0527fe337a37a4b6dbf1c34
mswift42 <martin.haesler@gmail.com>
2016-02-14 11:18:29 +0000

    import dashboard component

--- dtour/lib/app_component.dart
+++ dtour/lib/app_component.dart
@@ -1,13 +1,29 @@
 import 'package:angular2/core.dart';
-import 'package:dtour/heroes_component.dart';
+import 'package:angular2/router.dart';
+import 'package:dtour/hero_service.dart' show HeroService;
+import 'package:dtour/heroes_component.dart' show HeroesComponent;
+import 'package:dtour/dashboard_component.dart' show DashboardComponent;
 
 @Component(
     selector: 'my-app',
     template: '''
 <h1>{{title}}</h1>
-<my-heroes></my-heroes>
+<nav>
+<a [routerLink]="['Heroes']">Heroes</a>
+<a [routerLink]="['Dashboard']">Dashboard</a>
+</nav>
+<router-outlet></router-outlet>
 ''',
-    directives: const [HeroesComponent])
+    directives: const [ROUTER_DIRECTIVES],
+    providers: const [ROUTER_PROVIDERS, HeroService])
+@RouteConfig(const [
+  const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent),
+  const Route(
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: DashboardComponent,
+      useAsDefault: true)
+])
 class AppComponent {
-  var title = 'Tour of Heroes';
+  final String title = 'Tour of Heroes';
 }
