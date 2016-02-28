27ed6888073fc9abf7317f49e5ea22e5380bc73b
mswift42 <martin.haesler@gmail.com>
2016-02-28 13:34:52 +0000

    add app folder

---
+++ dts/app/components/app-component/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector:'task-app',
+  templateUrl: 'app/components/app-component/app.component.html'
+})
+
+export class AppComponent {}
