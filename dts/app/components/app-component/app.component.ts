3c615dac856dd77e6b18b7ae7d07946c72c8710b
mswift42 <martin.haesler@gmail.com>
2016-03-04 17:16:51 +0000

    iterate over tasks

--- dts/app/components/app-component/app.component.ts
+++ dts/app/components/app-component/app.component.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {TaskListComponent} from '../tasklist_component/tasklist.component';
 
 @Component({
   selector:'task-app',
-  templateUrl: 'app/components/app-component/app.component.html'
+  templateUrl: 'app/components/app-component/app.component.html',
+  directives: [TaskListComponent]
 })
 
 export class AppComponent {}
