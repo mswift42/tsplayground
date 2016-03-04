61d791c6d2da22aa6dfdccdb8603693ae2a5fcc1
mswift42 <martin.haesler@gmail.com>
2016-03-04 15:56:05 +0000

    add component properties.

--- dts/app/components/task_component/task.component.ts
+++ dts/app/components/task_component/task.component.ts
@@ -2,7 +2,14 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'single-task',
-  templateUrl: 'app/components/task_component/task.component.html'
+  templateUrl: 'app/components/task_component/task.component.html',
+  inputs: ['summary', 'taskid', 'scheduled', 'taskcontents']
 })
 
-  export class TaskComponent {}
+export class TaskComponent {
+  summary: string;
+  taskid: string;
+  scheduled: Date;
+  taskcontents: string[];
+
+}
