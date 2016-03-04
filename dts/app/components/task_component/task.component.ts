c74a3a997a1e3b18e5865751f0d9dd235e1e7783
mswift42 <martin.haesler@gmail.com>
2016-03-04 15:50:46 +0000

    add task_component folder

---
+++ dts/app/components/task_component/task.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'single-task',
+  templateUrl: 'app/components/task_component/task.component.html'
+})
+
+  export class TaskComponent {}
